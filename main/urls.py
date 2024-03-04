8e22adea83df2e971760d578c64ba16ddd39fe6f
GAlmazR <almaz78.almaz@yandex.ru>
2024-03-04 19:32:50 +0300

    Delete excess files

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,4 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('time/', views.date_t, name='time'),
-    path('script/', views.script_view, name='script'),
 ]
--- main/views.py
+++ main/views.py
@@ -3,11 +3,3 @@ from datetime import datetime
 
 def home(request: HttpRequest):
     return HttpResponse(f'''<h1>Hello, {request.user.email}</h1>''')
-
-def date_t(request: HttpRequest):
-    return HttpResponse(datetime.now())
-
-def script_view(request: HttpRequest):
-    return HttpResponse('''
-        <script>alert("hello")</script>
-    ''')
