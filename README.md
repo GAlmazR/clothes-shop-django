152f4869e2bf0747afabd063683bf124ffc82d84
GAlmazR <almaz78.almaz@yandex.ru>
2024-03-18 21:26:41 +0300

    Добавлены и изменены файлы

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ pip install -r requirements.txt
 Создать файл .env
 SECRET="6a0kj6z06h2(m$*)111_-(tq2_c+^+^k$&=8w8*s4r9*8j6v+h"
 
+python manage.py makemigrations
+
 python manage.py migrate
 
 python manage.py createsuperuser --username admin --email admin@domain.com
--- main/views.py
+++ main/views.py
@@ -3,8 +3,11 @@ from django.shortcuts import render
 
 from .models import Product
 
+
 def index_view(request: HttpRequest):
-    products = Product.objects.all()
+    products = Product.objects.filter(is_active=True)
+    products = products.order_by('-count')
+
     return HttpResponse(render(request, 'products.html', {
         'products': products
     }))
